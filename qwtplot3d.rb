c9b4cb0eaf9871e643b588b24f9608fbf7ebadd5
gcoco <gcoco@users.noreply.github.com>
2013-11-26 13:34:45 -0500

    Update qwtplot3d.rb

--- qwtplot3d.rb
+++ qwtplot3d.rb
@@ -3,8 +3,7 @@ require 'formula'
 class Qwtplot3d < Formula
   homepage 'http://qwtplot3d.sourceforge.net/'
   url 'http://downloads.sourceforge.net/sourceforge/qwtplot3d/qwtplot3d-0.2.7.zip'
-  #url 'http://downloads.sourceforge.net/sourceforge/qwtplot3d/qwtplot3d-0.2.7.tgz'
-  #sha1 '4463fafb8420a91825e165da7a296aaabd70abea'
+  sha1 '17f55d37ac93fd17d7f3f6b90031ceb796bfe6d1'
 
   #depends_on 'qt' => :build
   
@@ -19,8 +18,8 @@ class Qwtplot3d < Formula
 end
 
 __END__
---- a/qwtplot3d-0.2.7/qwtplot3d/src/qwt3d_function.cpp	2007-08-23 12:20:56.000000000 +0200
-+++ b/qwtplot3d-0.2.7/qwtplot3d/src/qwt3d_function.cpp	2009-11-14 14:04:57.000000000 +0100
+--- a/qwtplot3d/src/qwt3d_function.cpp	2007-08-23 12:20:56.000000000 +0200
++++ b/qwtplot3d/src/qwt3d_function.cpp	2009-11-14 14:04:57.000000000 +0100
 @@ -1,3 +1,4 @@
 +#include <cstdio>
  #include "qwt3d_surfaceplot.h"
