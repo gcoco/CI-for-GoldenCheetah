3428f7fc468ca36408b3a4aaea60029f21713bdc
gcoco <gcoco@users.noreply.github.com>
2013-11-26 10:42:20 -0500

    Update qwtplot3d.rb

--- qwtplot3d.rb
+++ qwtplot3d.rb
@@ -3,6 +3,7 @@ require 'formula'
 class Qwtplot3d < Formula
   homepage 'http://qwtplot3d.sourceforge.net/'
   url 'http://downloads.sourceforge.net/sourceforge/qwtplot3d/qwtplot3d-0.2.7.tgz'
+  sha1 '4463fafb8420a91825e165da7a296aaabd70abea'
 
   def patches
     DATA
@@ -17,7 +18,7 @@ end
 __END__
 # Cherry-picked from:
 # https://trac.macports.org/browser/trunk/dports/graphics/qwtplot3d/files/patch-qwtplot3d.pro.diff?rev=94241 
--- qwtplot3d.pro.orig 2010-11-02 15:48:06.000000000 -0400
+--- qwtplot3d.pro 2010-11-02 15:48:06.000000000 -0400
 +++ qwtplot3d.pro 2010-11-02 15:50:49.000000000 -0400
 @@ -3,7 +3,7 @@
  TARGET            = qwtplot3d
