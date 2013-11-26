f600b1a937d2551b67fe2791739e70a55eea4989
gcoco <gcoco@users.noreply.github.com>
2013-11-26 13:04:36 -0500

    Update qwtplot3d.rb

--- qwtplot3d.rb
+++ qwtplot3d.rb
@@ -6,8 +6,7 @@ class Qwtplot3d < Formula
   sha1 '4463fafb8420a91825e165da7a296aaabd70abea'
 
   def patches
-  { :p0 => DATA }
-  #DATA
+    DATA
   end
   
   def install
@@ -17,8 +16,8 @@ class Qwtplot3d < Formula
 end
 
 __END__
---- qwtplot3d/src/qwt3d_function.cpp	2007-08-23 12:20:56.000000000 +0200
-+++ qwtplot3d/src/qwt3d_function.cpp	2009-11-14 14:04:57.000000000 +0100
+--- a/qwtplot3d-0.2.7/qwtplot3d/src/qwt3d_function.cpp	2007-08-23 12:20:56.000000000 +0200
++++ b/qwtplot3d-0.2.7/qwtplot3d/src/qwt3d_function.cpp	2009-11-14 14:04:57.000000000 +0100
 @@ -1,3 +1,4 @@
 +#include <cstdio>
  #include "qwt3d_surfaceplot.h"
