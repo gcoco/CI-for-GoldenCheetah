a285cedcf07f58d9246479e5ca0cbb74b3f0c008
gcoco <gcoco@users.noreply.github.com>
2013-12-04 14:17:00 -0500

    Update qwtplot3d.rb

--- qwtplot3d.rb
+++ qwtplot3d.rb
@@ -10,7 +10,7 @@ class Qwtplot3d < Formula
 #  depends_on 'qt'
   
   def patches
-    { :"-dry-run --unified" => [
+    { :"-dry-run" => [
     #"https://raw.github.com/gcoco/CI-for-GoldenCheetah/master/qwtplot3d.pro.patch"
     DATA
     ]}
