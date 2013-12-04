07f36e2d4221583a11192e556559b378d8c3ff98
gcoco <gcoco@users.noreply.github.com>
2013-12-04 13:23:30 -0500

    Update qwtplot3d.rb

--- qwtplot3d.rb
+++ qwtplot3d.rb
@@ -8,7 +8,7 @@ class Qwtplot3d < Formula
 #  depends_on 'qt'
   
   def patches
-    { :p1 => [
+    { :verbose => [
     #"https://raw.github.com/gcoco/CI-for-GoldenCheetah/master/qwtplot3d.pro.patch"
     DATA
     ]}
