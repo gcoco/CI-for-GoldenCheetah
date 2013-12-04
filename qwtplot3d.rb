5596607a84c749c3e93b072ce46d6af4b5370e1e
gcoco <gcoco@users.noreply.github.com>
2013-12-04 16:39:59 -0500

    Update qwtplot3d.rb

--- qwtplot3d.rb
+++ qwtplot3d.rb
@@ -7,7 +7,7 @@ class Qwtplot3d < Formula
   url 'http://downloads.sourceforge.net/sourceforge/qwtplot3d/qwtplot3d-0.2.7.tgz'
   sha1 '4463fafb8420a91825e165da7a296aaabd70abea'
 
-  #depends_on 'qt'
+  depends_on 'qt', 'perl' :build
   
 #  def patches
 #    { :"-debug=3" => [
