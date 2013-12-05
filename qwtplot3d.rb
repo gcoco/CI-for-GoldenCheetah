db47f04f54117179441a00d8ed9cded3de29e79c
gcoco <gcoco@users.noreply.github.com>
2013-12-05 11:44:20 -0500

    Update qwtplot3d.rb

--- qwtplot3d.rb
+++ qwtplot3d.rb
@@ -2,15 +2,14 @@ require 'formula'
 
 class Qwtplot3d < Formula
   homepage 'http://qwtplot3d.sourceforge.net/'
-#  url 'http://downloads.sourceforge.net/sourceforge/qwtplot3d/qwtplot3d-0.2.7.zip'
-#  sha1 '17f55d37ac93fd17d7f3f6b90031ceb796bfe6d1'
   url 'http://downloads.sourceforge.net/sourceforge/qwtplot3d/qwtplot3d-0.2.7.tgz'
   sha1 '4463fafb8420a91825e165da7a296aaabd70abea'
 
   depends_on 'qt'
 
   def patches
-    { :"-debug=3" => [
+    { 
+      [
         "https://trac.macports.org/export/114349/trunk/dports/graphics/qwtplot3d/files/patch-qwtplot3d.pro.diff"
       ],
       :p0 => [
@@ -20,21 +19,8 @@ class Qwtplot3d < Formula
   end
   
   def install
-    system "qmake -makefile -spec unsupported/macx-clang CONFIG+=release"
-    system 'make'
+    system 'qmake -makefile -spec unsupported/macx-clang'
+    system 'make install'
+    system 'ls -laR /usr/local'
   end
 end
-
-__END__
-diff --git a/qwtplot3d.pro b/qwtplot3d.pro.diff
-index 4060fe8..b0e8aa6 100644
---- a/qwtplot3d.pro
-+++ b/qwtplot3d.pro.diff
-@@ -3,7 +3,7 @@
- 
- TARGET            = qwtplot3d
- TEMPLATE          = lib
--CONFIG           += qt warn_on opengl thread zlib debug
-+CONFIG           += qt warn_on opengl thread zlib release
- MOC_DIR           = tmp
- OBJECTS_DIR       = tmp
