62a63c63ad69ffe8d08b48225486fbf8e6974e08
gcoco <gcoco@users.noreply.github.com>
2013-12-05 10:59:54 -0500

    Update qwtplot3d.rb

--- qwtplot3d.rb
+++ qwtplot3d.rb
@@ -13,10 +13,13 @@ class Qwtplot3d < Formula
     { :"-debug=3" => [
       "https://trac.macports.org/export/114349/trunk/dports/graphics/qwtplot3d/files/patch-qwtplot3d.pro.diff"
       ]
+    }
+    {
       :p0 => [
       "https://trac.macports.org/export/114349/trunk/dports/graphics/qwtplot3d/files/patch-include_qwt3d_openglhelper.h.diff"
 #    #DATA
-    ]}
+       ]
+    }
   end
   
   def install
