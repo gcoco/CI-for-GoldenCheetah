29fad28e334526b15b321378bc4a4ffafd765918
gcoco <gcoco@users.noreply.github.com>
2013-12-18 11:54:04 -0500

    Update qwtplot3d.rb

--- qwtplot3d.rb
+++ qwtplot3d.rb
@@ -29,10 +29,8 @@ end
 
 
 __END__
-diff --git qwtplot3d.pro qwtplot3d.pro
-index 4060fe8..b0e8aa6 100644
---- qwtplot3d.pro
-+++ qwtplot3d.pro
+--- a/qwtplot3d.pro
++++ b/qwtplot3d.pro
 @@ -3,7 +3,7 @@
  
  TARGET            = qwtplot3d
@@ -42,23 +40,3 @@ index 4060fe8..b0e8aa6 100644
  MOC_DIR           = tmp
  OBJECTS_DIR       = tmp
  INCLUDEPATH       = include
-@@ -21,7 +21,7 @@ win32:QMAKE_CXXFLAGS     += $$QMAKE_CFLAGS_STL
- win32:CONFIG -= zlib
- 
- linux-g++:TMAKE_CXXFLAGS += -fno-exceptions
--unix:VERSION = 0.2.6
-+unix:VERSION = 0.2.7
- 
- # Input
- SOURCES += src/qwt3d_axis.cpp \
-@@ -93,3 +93,10 @@ zlib {
-   win32:LIBS += zlib.lib
- 	unix:LIBS  += -lz
- }
-+
-+target.path    = $$INSTALLBASE/lib 
-+headers.path   = $$INSTALLBASE/include/qwtplot3d-qt4 
-+doc.path       = $$INSTALLBASE/share/qwtplot3d-qt4/doc 
-+headers.files  = $$HEADERS 
-+
-+INSTALLS       = target headers doc
