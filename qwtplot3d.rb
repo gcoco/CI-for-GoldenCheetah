0b777aab921f7ebe48ef6307ab09e4a957d3af07
gcoco <gcoco@users.noreply.github.com>
2014-03-18 18:33:55 -0400

    Update qwtplot3d.rb

--- qwtplot3d.rb
+++ qwtplot3d.rb
@@ -24,7 +24,7 @@ class Qwtplot3d < Formula
 
   def install
     qt_path < #{Formula["#{qt_ver}"].opt_prefix}/bin/qmake
-    inreplace "qwtplot3d.pro", "TARGET            = qwtplot3d", "TARGET            = qwtplot3d-#{qt-ver}"
+    #inreplace "qwtplot3d.pro", "TARGET            = qwtplot3d", "TARGET            = qwtplot3d-#{qt-ver}"
     inreplace "qwtplot3d.pro", "$$INSTALLBASE", "#{prefix}"
     system "cat qwtplot3d.pro"
     system "#{qt_path}/bin/qmake"
