4f39868f7d1bf518e8220e47234a8f496065a419
gcoco <gcoco@users.noreply.github.com>
2014-03-18 20:51:26 -0400

    Update qwtplot3d.rb

--- qwtplot3d.rb
+++ qwtplot3d.rb
@@ -8,7 +8,7 @@ class Qwtplot3d < Formula
   option 'with-qt5', 'Build using Qt5 backend'
   
   if build.with? 'qt5'
-    qt_ver < ( build.with?('qt5') ? 'qt5' : 'qt4' )
+    qt_ver  ( build.with?('qt5') ? 'qt5' : 'qt4' )
   end
   
   depends_on "#{qt_ver}"
@@ -23,7 +23,7 @@ class Qwtplot3d < Formula
   end
 
   def install
-    qt_path = '#{Formula["#{qt_ver}"].opt_prefix}/bin/qmake'
+    qt_path = "#{Formula['#{qt_ver}'].opt_prefix}/bin/qmake"
     inreplace "qwtplot3d.pro", "TARGET            = qwtplot3d", "TARGET            = qwtplot3d-#{qt-ver}"
     inreplace "qwtplot3d.pro", "$$INSTALLBASE", "#{prefix}"
     system "cat qwtplot3d.pro"
