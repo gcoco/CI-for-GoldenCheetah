ea6bf7168febcffbf3e2b8f76b29bb3594a67a37
gcoco <gcoco@users.noreply.github.com>
2014-03-18 20:55:08 -0400

    Update qwtplot3d.rb

--- qwtplot3d.rb
+++ qwtplot3d.rb
@@ -7,9 +7,7 @@ class Qwtplot3d < Formula
 
   option 'with-qt5', 'Build using Qt5 backend'
   
-  if build.with? 'qt5'
-    qt_ver  ( build.with?('qt5') ? 'qt5' : 'qt4' )
-  end
+  qt_ver = ( build.with?('qt5') ? 'qt5' : 'qt4' )
   
   depends_on "#{qt_ver}"
 
