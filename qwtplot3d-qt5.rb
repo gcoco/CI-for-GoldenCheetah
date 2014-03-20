e732f1416729340978bee265e071da7e4dc7dd3b
gcoco <gcoco@users.noreply.github.com>
2014-03-20 10:14:07 -0400

    Update qwtplot3d-qt5.rb

--- qwtplot3d-qt5.rb
+++ qwtplot3d-qt5.rb
@@ -5,7 +5,7 @@ class Qwtplot3dqt5 < Formula
   url 'http://downloads.sourceforge.net/sourceforge/qwtplot3d/qwtplot3d-0.2.7.tgz'
   sha1 '4463fafb8420a91825e165da7a296aaabd70abea'
 
-  depends_on qt5
+  depends_on 'qt5'
 
   def patches
     {
