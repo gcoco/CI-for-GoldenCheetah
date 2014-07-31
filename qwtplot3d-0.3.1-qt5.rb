98fec3648e2a91e6bd3452b57c4a907b7b3e6871
gcoco <gcoco@users.noreply.github.com>
2014-07-31 08:48:46 -0400

    Update qwtplot3d-0.3.1-qt5.rb

--- qwtplot3d-0.3.1-qt5.rb
+++ qwtplot3d-0.3.1-qt5.rb
@@ -3,6 +3,7 @@ require 'formula'
 class Qwtplot3d031Qt5 < Formula
   homepage 'http://qwtplot3d.sourceforge.net/'
   url 'http://sintegrial.com/get.php?file=qwtplot3d_src'
+  sha1 'e4ffbbe893359d3069475a1bdaa03fca302fc70d'
   version '0.3.1'
 
   depends_on 'qt5'
@@ -12,10 +13,10 @@ class Qwtplot3d031Qt5 < Formula
     inreplace "qwtplot3d.pro", "qwtplot3d", "qwtplot3d-qt5"
     inreplace "qwtplot3d.pro", "static", "release"
     inreplace "qwtplot3d.pro", "0.3.0", "0.3.1"
-    system "echo target.path    = $$INSTALLBASE/lib >> qwtplot3d.pro"
-    system "echo headers.path   = $$INSTALLBASE/include/qwtplot3d >> qwtplot3d.pro"
-    system "echo doc.path       = $$INSTALLBASE/share/qwt/doc >> qwtplot3d.pro"
-    system "echo headers.files  = $$HEADERS >> qwtplot3d.pro"
+    system "echo target.path    = \$\$INSTALLBASE/lib >> qwtplot3d.pro"
+    system "echo headers.path   = \$\$INSTALLBASE/include/qwtplot3d >> qwtplot3d.pro"
+    system "echo doc.path       = \$\$INSTALLBASE/share/qwt/doc >> qwtplot3d.pro"
+    system "echo headers.files  = \$\$HEADERS >> qwtplot3d.pro"
     system "echo INSTALLS       = target headers doc >> qwtplot3d.pro"
     inreplace "qwtplot3d.pro", "$$INSTALLBASE", prefix
     system "#{Formula['qt5'].opt_prefix}/bin/qmake"
