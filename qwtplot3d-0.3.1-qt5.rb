494d93e5758dc381005da48695be17a4f876318e
gcoco <gcoco@users.noreply.github.com>
2014-07-31 09:49:21 -0400

    Update qwtplot3d-0.3.1-qt5.rb

--- qwtplot3d-0.3.1-qt5.rb
+++ qwtplot3d-0.3.1-qt5.rb
@@ -10,13 +10,14 @@ class Qwtplot3d031Qt5 < Formula
 
 
   def install
+    system "cat include/qwt3d_openglhelper.h.orig"
     inreplace "qwtplot3d.pro", "qwtplot3d", "qwtplot3d-qt5"
     inreplace "qwtplot3d.pro", "static", "release"
     inreplace "qwtplot3d.pro", "0.3.0", "0.3.1"
-    system 'echo "target.path    = $$INSTALLBASE/lib" >> qwtplot3d.pro'
+    system "echo target.path    = \\$\\$INSTALLBASE/lib >> qwtplot3d.pro"
     system "echo headers.path   = \\$\\$INSTALLBASE/include/qwtplot3d >> qwtplot3d.pro"
-    system 'echo "doc.path       = \$\$INSTALLBASE/share/qwt/doc" >> qwtplot3d.pro'
-    system "echo headers.files  = \$\$HEADERS >> qwtplot3d.pro"
+    system 'echo doc.path       = \\$\\$INSTALLBASE/share/qwt/doc" >> qwtplot3d.pro"
+    system "echo headers.files  = \\$\\$HEADERS >> qwtplot3d.pro"
     system "echo INSTALLS       = target headers doc >> qwtplot3d.pro"
     inreplace "qwtplot3d.pro", "$$INSTALLBASE", prefix
     system "#{Formula['qt5'].opt_prefix}/bin/qmake"
