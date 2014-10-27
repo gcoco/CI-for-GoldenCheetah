e9cad88331ff1798fe23e3088993c1575ed51f1b
gcoco <gcoco@users.noreply.github.com>
2014-10-27 13:09:41 -0400

    Update qwtplot3d-0.3.1-qt5.rb

--- qwtplot3d-0.3.1-qt5.rb
+++ qwtplot3d-0.3.1-qt5.rb
@@ -1,17 +1,17 @@
 require 'formula'
 
 class Qwtplot3d031Qt5 < Formula
-  homepage 'http://qwtplot3d.sourceforge.net/'
-  url 'http://sintegrial.com/get.php?file=qwtplot3d_src'
+  homepage 'http://sintegrial.com/qwtplot'
+  url 'http://sintegrial.com/files/qwtplot3d-0.3.1-322-src.zip'
   sha1 'e4ffbbe893359d3069475a1bdaa03fca302fc70d'
-  version '0.3.1'
+  version '0.3.1_322'
 
   depends_on 'qt5'
   
   def install
     inreplace "qwtplot3d.pro", "qwtplot3d", "qwtplot3d-qt5"
     inreplace "qwtplot3d.pro", "static", "release"
-    inreplace "qwtplot3d.pro", "0.3.0", "0.3.1"
+    inreplace "qwtplot3d.pro", "0.3.0", "0.3.1_322"
     inreplace "include/qwt3d_openglhelper.h", "GL/glu.h", "OpenGL/glu.h"
     system "echo target.path    = \\$\\$INSTALLBASE/lib >> qwtplot3d.pro"
     system "echo headers.path   = \\$\\$INSTALLBASE/include/qwtplot3d-qt5 >> qwtplot3d.pro"
