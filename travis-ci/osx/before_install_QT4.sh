1b4bb0ba9f15cf4ce4f1a51a656a17a1a990a4b2
GC <gc@localhost.localdomain>
2015-06-05 21:26:42 -0400

    OSX QT4 before_install

--- travis-ci/osx/before_install_QT4.sh
+++ travis-ci/osx/before_install_QT4.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-
-echo
-echo "# arguments called with ---->  ${@}     "
-echo "# \$1 ---------------------->  $1       "
-echo "# \$2 ---------------------->  $2       "
-echo "# path to me --------------->  ${0}     "
-echo "# parent path -------------->  ${0%/*}  "
-echo "# my name ------------------>  ${0##*/} "
-echo
+brew update
+brew install qt$QT
+brew install libical
+brew install libusb libusb-compat
+brew install srmio
+#  - brew install --HEAD https://raw.github.com/gcoco/CI-for-GoldenCheetah/master/libkml.rb
+sudo chmod -R +w /usr/local
+git clone --depth 1 https://github.com/sintegrial/qwtplot3d.git qwtplot3d
+cd qwtplot3d
+CC=clang CXX=clang++ /usr/local/opt/qt$QT/bin/qmake -makefile -recursive QMAKE_CXXFLAGS_WARN_ON+=-Wno-unused-private-field
+CC=clang CXX=clang++ make -j2 
+cd ..
+git clone --branch 0.98 https://github.com/kypeli/kQOAuth.git kQOAuth-0.98
+cd kQOAuth-0.98
+CC=clang CXX=clang++ /usr/local/opt/qt$QT/bin/qmake -makefile -recursive QMAKE_CXXFLAGS_WARN_ON+=-Wno-unused-private-field
+CC=clang CXX=clang++ make -j2 qmake_all 
+CC=clang CXX=clang++ sudo make install
+cd ..
+curl -O http://www.ftdichip.com/Drivers/D2XX/MacOSX/D2XX1.2.2.dmg
+hdiutil mount D2XX1.2.2.dmg
 exit
