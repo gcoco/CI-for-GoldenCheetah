255c0d393d763a8946152643016d3be6d85bcad4
gcoco <gcoco@users.noreply.github.com>
2015-09-13 19:22:25 -0400

    git checkout not git reset

--- install-qt.sh
+++ install-qt.sh
@@ -2,6 +2,6 @@
 if [ "$QT" = "qt5" ]
 then
   # Select QT 5.4.2
-  git reset 00e46351980 $( brew --prefix )/Library/Formula/qt5.rb
+  git checkout 00e46351980 $( brew --prefix )/Library/Formula/qt5.rb
 fi
 brew install $QT
