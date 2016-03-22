9fcfe51b33bb216c6d5325ccf8a77394ea084c42
gcoco <gcoco@users.noreply.github.com>
2016-03-22 10:41:39 -0400

    Add libs needed for this build
    [ci skip]

--- travis/linux/install-libusb-compat.sh
+++ travis/linux/install-libusb-compat.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 set -e
+sudo apt-get install -qq libusb-1.0-0-dev libudev-dev
 wget https://sourceforge.net/projects/libusb/files/libusb-compat-0.1/libusb-compat-0.1.5/libusb-compat-0.1.5.tar.bz2/download -O libusb-compat-0.1.5.tar.bz2
 tar xf libusb-compat-0.1.5.tar.bz2
 cd libusb-compat-0.1.5
