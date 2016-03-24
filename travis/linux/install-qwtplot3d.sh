f29abfd6c67a351603343cc3a5f2a3355f6b0ee1
gcoco <gcoco@users.noreply.github.com>
2016-03-23 23:38:50 -0400

    Test change to qwtplot3d

--- travis/linux/install-qwtplot3d.sh
+++ travis/linux/install-qwtplot3d.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-git clone --depth 1 https://github.com/sintegrial/qwtplot3d.git qwtplot3d
+# git clone --depth 1 https://github.com/sintegrial/qwtplot3d.git qwtplot3d
+git clone --depth 1 --branch Qt5.6 https://github.com/gcoco/qwtplot3d.git qwtplot3d
 cd qwtplot3d
 sed -i "s|examples||" qwtplot3d.pro
 qmake -recursive
