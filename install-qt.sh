f20bb13cf3af36cdb3ba0eb28ef0dd78b45b308f
gcoco <gcoco@users.noreply.github.com>
2016-03-24 00:42:28 -0400

    brew QT install
    Set variables:
    QT=4 => Brew QT 4 latest
    QT=5 => Brew QT 5 latest
    QT=5 QT_VER=5.5 => Brew Qt 5.5.1_2
    [ci skip]

---
+++ install-qt.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ "$QT_VER" = "5.5" ]
+then
+  pwd
+  workdir=`pwd`
+  echo ${workdir}
+  cd $( brew --prefix )
+  pwd
+  # Select QT 5.5.1
+  git checkout fb64f6cd91ff Library/Formula/qt5.rb
+  cd ${workdir}
+  pwd
+fi
+brew install $QT
