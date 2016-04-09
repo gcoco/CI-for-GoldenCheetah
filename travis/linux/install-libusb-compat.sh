24b4862310da6958216979fb4851469765035bca
gcoco <gcoco@users.noreply.github.com>
2016-04-08 20:45:58 -0400

    Update install-libusb-compat.sh
    [ci skip]

--- travis/linux/install-libusb-compat.sh
+++ travis/linux/install-libusb-compat.sh
@@ -5,5 +5,5 @@ wget https://sourceforge.net/projects/libusb/files/libusb-compat-0.1/libusb-comp
 tar xf libusb-compat-0.1.5.tar.bz2
 cd libusb-compat-0.1.5
 ./configure --disable-shared --enable-static --prefix=`cd ..; pwd`/usr
-make -j3 install 
+make V=0 -j3 install 
 cd ..
