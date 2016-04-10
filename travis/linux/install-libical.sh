c80dc8350f97dc3b2ff7f75ea748f25e373f9141
gcoco <gcoco@users.noreply.github.com>
2016-04-10 06:50:54 -0400

    Update install-libical.sh
    [ci skip]

--- travis/linux/install-libical.sh
+++ travis/linux/install-libical.sh
@@ -5,5 +5,5 @@ tar xf libical-1.0.1.tar.gz
 cd libical-1.0.1
 mkdir build && cd build
 cmake .. -DSTATIC_ONLY=true -DCMAKE_INSTALL_PREFIX=`cd ../..; pwd;`/usr
-make V=0 install
+make --silent install
 cd ../..
