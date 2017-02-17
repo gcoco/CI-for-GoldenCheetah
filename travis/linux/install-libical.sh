27769abf152abd1ee429fdf3b862b2d9131f72e0
gcoco <gcoco@users.noreply.github.com>
2017-02-16 20:23:24 -0500

    Update install-libical.sh

--- travis/linux/install-libical.sh
+++ travis/linux/install-libical.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
-wget https://github.com/libical/libical/releases/download/v1.0.1/libical-1.0.1.tar.gz -O libical-1.0.1.tar.gz
-tar xf libical-1.0.1.tar.gz
-cd libical-1.0.1
+wget https://github.com/libical/libical/releases/download/v2.0.0/libical-2.0.0.tar.gz -O libical-2.0.0.tar.gz
+tar xf libical-2.0.0.tar.gz
+cd libical-2.0.0
 mkdir build && cd build
 cmake .. -DSTATIC_ONLY=true -DCMAKE_INSTALL_PREFIX=`cd ../..; pwd;`/usr
 make --silent install
