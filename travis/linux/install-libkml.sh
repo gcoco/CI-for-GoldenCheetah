63178ec347d506036a5d06f8db7928fe56d4d34d
gcoco <gcoco@users.noreply.github.com>
2017-03-16 10:28:48 -0400

    Update install-libkml.sh
    [ci skip]

--- travis/linux/install-libkml.sh
+++ travis/linux/install-libkml.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
+cd ${TRAVIS_BUILD_DIR}
 wget https://github.com/libkml/libkml/archive/1.3.0.tar.gz -O libkml-1.3.0.tar.gz
 tar xf libkml-1.3.0.tar.gz
 cd libkml-1.3.0
 mkdir build && cd build
-cmake .. -DBUILD_SHARED_LIBS=off -DCMAKE_INSTALL_PREFIX=`cd ../..; pwd;`/usr
+cmake .. -DBUILD_SHARED_LIBS=off -DCMAKE_INSTALL_PREFIX=${TRAVIS_BUILD_DIR}/usr
 make --silent install
-cd ../..
+cd ${TRAVIS_BUILD_DIR}
