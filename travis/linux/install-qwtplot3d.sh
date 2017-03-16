13d6eae43d0498df70e640b73cfd9e27efd69777
gcoco <gcoco@users.noreply.github.com>
2017-03-16 10:27:08 -0400

    Update install-qwtplot3d.sh
    [ci skip]

--- travis/linux/install-qwtplot3d.sh
+++ travis/linux/install-qwtplot3d.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
+cd ${TRAVIS_BUILD_DIR}
 # git clone --depth 1 https://github.com/sintegrial/qwtplot3d.git qwtplot3d
 git clone --depth 1 --branch Qt5.6 https://github.com/gcoco/qwtplot3d.git qwtplot3d
 cd qwtplot3d
 sed -i "s|examples||" qwtplot3d.pro
 ${QTPATH}/bin/qmake -recursive
 make --silent -j3
-cd ..
+cd ${TRAVIS_BUILD_DIR}
