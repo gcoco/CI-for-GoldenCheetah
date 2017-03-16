1238e16cb58399ce3ae0738021d84e46d68ae18b
gcoco <gcoco@users.noreply.github.com>
2017-03-16 10:29:46 -0400

    Update install-lmfit.sh
    [ci skip]

--- travis/linux/install-lmfit.sh
+++ travis/linux/install-lmfit.sh
@@ -1,8 +1,9 @@
 #/bin/bash
 set -e
+cd ${TRAVIS_BUILD_DIR}
 wget http://apps.jcns.fz-juelich.de/src/lmfit/lmfit-6.1.tgz -O lmfit-6.1.tgz
 tar xf lmfit-6.1.tgz
 cd lmfit-6.1
-./configure --disable-shared --enable-static --prefix=`cd ..; pwd`/usr
+./configure --disable-shared --enable-static --prefix=`${TRAVIS_BUILD_DIR}/usr
 make --silent -j3 install 
-cd ..
+cd ${TRAVIS_BUILD_DIR}
