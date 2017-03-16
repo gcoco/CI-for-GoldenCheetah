b051f8222e81e473c8e5ae76935ad2cdea3d36b7
gcoco <gcoco@users.noreply.github.com>
2017-03-16 10:31:07 -0400

    Update install-srmio.sh
    [ci skip]

--- travis/linux/install-srmio.sh
+++ travis/linux/install-srmio.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
+cd ${TRAVIS_BUILD_DIR}
 wget http://www.zuto.de/project/files/srmio/srmio-0.1.1~git1.tar.gz -O srmio-0.1.1~git1.tar.gz
 tar xf srmio-0.1.1~git1.tar.gz
 cd srmio-0.1.1~git1
-./configure --disable-shared --enable-static --prefix=`cd ..; pwd`/usr
+./configure --disable-shared --enable-static --prefix=${TRAVIS_BUILD_DIR}/usr
 make --silent -j3 install
-cd ..
+cd ${TRAVIS_BUILD_DIR}
