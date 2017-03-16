835505e7da07f0f9a12b792b6c958812be682f35
gcoco <gcoco@users.noreply.github.com>
2017-03-16 10:29:19 -0400

    Update install-libusb-compat.sh
    [ci skip]

--- travis/linux/install-libusb-compat.sh
+++ travis/linux/install-libusb-compat.sh
@@ -1,9 +1,10 @@
 #/bin/bash
 set -e
+cd ${TRAVIS_BUILD_DIR}
 sudo apt-get install -qq libusb-1.0-0-dev libudev-dev
 wget https://sourceforge.net/projects/libusb/files/libusb-compat-0.1/libusb-compat-0.1.5/libusb-compat-0.1.5.tar.bz2/download -O libusb-compat-0.1.5.tar.bz2
 tar xf libusb-compat-0.1.5.tar.bz2
 cd libusb-compat-0.1.5
-./configure --disable-shared --enable-static --prefix=`cd ..; pwd`/usr
+./configure --disable-shared --enable-static --prefix=${TRAVIS_BUILD_DIR}/usr
 make --silent -j3 install 
-cd ..
+cd ${TRAVIS_BUILD_DIR}
