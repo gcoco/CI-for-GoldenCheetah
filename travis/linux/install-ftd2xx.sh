33e3d08f8b64417a3fe2d1b5044f3d7853f023ba
gcoco <gcoco@users.noreply.github.com>
2017-03-18 10:55:43 -0400

    Create install-ftd2xx.sh
    [ci skip]

---
+++ travis/linux/install-ftd2xx.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+cd ${TRAVIS_BUILD_DIR}
+wget http://www.ftdichip.com/Drivers/D2XX/Linux/libftd2xx-x86_64-1.3.6.tgz -O libftd2xx-x86_64-1.3.6.tgz
+tar xf libftd2xx-x86_64-1.3.6.tgz -C libftd2xx-x86_64-1.3.6
+cp libftd2xx-x86_64-1.3.6/release/build/libftd2xx.so.1.3.6 ${TRAVIS_BUILD_DIR}/usr/lib/libftd2xx.so
+cp libftd2xx-x86_64-1.3.6/release/*.h ${TRAVIS_BUILD_DIR}/usr/include
+cd ${TRAVIS_BUILD_DIR}
