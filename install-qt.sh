57ffd50480df37559e9f9c1a50970d135ac24551
gcoco <gcoco@users.noreply.github.com>
2017-04-01 17:08:12 -0400

    Allow versions 5.5.1, 5.6.2, 5.7.1 and 5.8.0
    Default to latest Qt5 if QT_VER is not set to one of the above.
    [ci skip]

--- install-qt.sh
+++ install-qt.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-if [ "$QT_VER" = "5.5" ]
+if [ "$QT_VER" = "5.5.1" ]
 then
-  pwd
-  workdir=`pwd`
-  echo ${workdir}
-  cd $( brew --prefix )
-  pwd
-  # Select QT 5.5.1
-  git checkout fb64f6cd91ff Library/Formula/qt5.rb
-  cd ${workdir}
-  pwd
+  brew install qt@5.5
 fi
-brew install $QT
+if [ "$QT_VER" = "5.6.2" ]
+then
+  brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/106d0878ee812cb8f5c403f717bdebe836445aee/Formula/qt5.rb
+fi
+if [ "$QT_VER" = "5.7.1" ]
+then
+  brew install qt@5.7
+fi
+if [ "$QT_VER" = "5.8.0" ]
+then
+  brew install qt5
+else
+  brew install qt5
+fi
